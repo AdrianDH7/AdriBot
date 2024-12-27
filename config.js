6a8c6a9ff596136c25abf4aa9b34f2c758d82cff
Niño Piña <ninopina10@gmail.com>
2024-12-27 15:31:09 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ import axios from 'axios'
 
 global.owner = [
   ['50557865603', 'ObisonBot-Ai', true],
+  ['19788774690', 'Vill Sombra', true]
 ]
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
