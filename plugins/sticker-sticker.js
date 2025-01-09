be5502d798e25ebb94c3e8278f199ec9e65e24b0
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-01-09 00:28:02 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,55 +1,95 @@
+const Asena = require("../Utilis/events")
+const { MessageType, Mimetype } = require("@adiwajshing/baileys")
+const Language = require("../language")
+const { webpToMp4 } = require("../Utilis/download")
+const { sticker, addExif } = require("../Utilis/fFmpeg")
+const { addAudioMetaData } = require("../Utilis/Misc")
+const Lang = Language.getString("sticker")
 
-import { sticker } from '../lib/sticker.js'
-//import uploadFile from '../lib/uploadFile.js'
-//import uploadImage from '../lib/uploadImage.js'
-//import { webp2png } from '../lib/webp2mp4.js'
+Asena.addCommand(
+  { pattern: "sticker ?(.*)", fromMe: true, desc: Lang.STICKER_DESC },
+  async (message, match) => {
+    if (
+      !message.reply_message ||
+      (!message.reply_message.video && !message.reply_message.image)
+    )
+      return await message.sendMessage(Lang.NEED_REPLY)
+    return await message.sendMessage(
+      await sticker(
+        "imagesticker",
+        await message.reply_message.downloadAndSaveMediaMessage("sticker"),
+        message.reply_message.image
+          ? 1
+          : message.reply_message.seconds < 10
+          ? 2
+          : 3,
+        match
+      ),
+      {
+        mimetype: Mimetype.webp,
+        quoted: message.quoted,
+        isAnimated: message.reply_message.video,
+      },
+      MessageType.sticker
+    )
+  }
+)
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+Asena.addCommand(
+  { pattern: "mp4", fromMe: true, desc: Lang.MP4_DESC },
+  async (message, match) => {
+    if (
+      !message.reply_message.sticker ||
+      !message.reply_message ||
+      !message.reply_message.animated
+    )
+      return await message.sendMessage(Lang.MP4_NEED_REPLY)
+    return await message.sendMessage(
+      await webpToMp4(
+        await message.reply_message.downloadAndSaveMediaMessage("mp4")
+      ),
+      { quoted: message.quoted },
+      MessageType.video
+    )
+  }
+)
 
-let stiker = false
-try {
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ''
-if (/webp|image|video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply(`☁️ *¡El video no puede durar mas de 8 segundos!*`)
-let img = await q.download?.()
-
-if (!img) return conn.reply(m.chat, `${emojis} *_Y el video ?, intenta enviar primero imagen/video/gif y luego responde con el comando._*`, m, rcanal)
-
-let out
-try {
-stiker = await sticker(img, false, global.packname, global.author)
-} catch (e) {
-console.error(e)
-} finally {
-if (!stiker) {
-if (/webp/g.test(mime)) out = await webp2png(img)
-else if (/image/g.test(mime)) out = await uploadImage(img)
-else if (/video/g.test(mime)) out = await uploadFile(img)
-if (typeof out !== 'string') out = await uploadImage(img)
-stiker = await sticker(false, out, global.packname, global.author)
-}}
-} else if (args[0]) {
-if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-
-else return m.reply(`💫 El url es incorrecto`)
-
-}
-} catch (e) {
-console.error(e)
-if (!stiker) stiker = e
-} finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: `CrowBot - ST ${emojis}`, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
-
-else return conn.reply(m.chat, `${emojis} *_La conversión ha fallado, intenta enviar primero imagen/video/gif y luego responde con el comando._*\n\n> ${emojis} ${publi} ${emojis}`, m, rcanal)
-
-
-}}
-handler.help = ['stiker <img>', 'sticker <url>']
-handler.tags = ['sticker']
-handler.command = ['s', 'sticker', 'stiker']
-
-export default handler
-
-const isUrl = (text) => {
-return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))} 
+Asena.addCommand(
+  { pattern: "take ?(.*)", fromMe: true, desc: Lang.TAKE_DESC },
+  async (message, match) => {
+    if (
+      !message.reply_message ||
+      (!message.reply_message.sticker && !message.reply_message.audio)
+    )
+      return await message.sendMessage("*Reply to a sticker or audio!*")
+    if (message.reply_message.sticker)
+      return await message.sendMessage(
+        await addExif(
+          await message.reply_message.downloadAndSaveMediaMessage("take"),
+          match
+        ),
+        {
+          mimetype: Mimetype.webp,
+          isAnimated: message.reply_message.animated,
+          quoted: message.quoted,
+        },
+        MessageType.sticker
+      )
+    if (!match)
+      return await message.sendMessage(
+        `*Give me title,artists,url*\n*aritists or url is optional*`
+      )
+    const [title, artists, url] = match.split(",")
+    return await message.sendMessage(
+      await addAudioMetaData(
+        await message.reply_message.downloadMediaMessage(),
+        title,
+        artists,
+        "",
+        url
+      ),
+      { quoted: message.quoted, mimetype: Mimetype.mp4Audio },
+      MessageType.audio
+    )
+  }
+)
