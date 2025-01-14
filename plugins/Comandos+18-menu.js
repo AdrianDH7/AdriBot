04576a541979e08a786b09442e93876ef78ec1d3
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-01-14 09:57:57 -0600

    Actualizar Comandos+18-menu.js

--- plugins/Comandos+18-menu.js
+++ plugins/Comandos+18-menu.js
@@ -5,66 +5,66 @@ import { join } from 'path'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command, args, usedPrefix: _p, __dirname, isOwner, text, isAdmin, isROwner }) => {
-if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `Los comandos +18 están desactivados. Usa #on para activar.`
-try {
-let { exp, limit, level, role } = global.db.data.users[m.sender]
-let { min, xp, max } = xpRange(level, global.multiplier)
+    if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `Los comandos +18 están desactivados. Usa #on para activar.`
+    try {
+        let { exp, limit, level, role } = global.db.data.users[m.sender]
+        let { min, xp, max } = xpRange(level, global.multiplier)
 
-let d = new Date(new Date + 3600000)
-let locale = 'es'
-let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
-let week = d.toLocaleDateString(locale, { weekday: 'long' })
-let date = d.toLocaleDateString(locale, {
-day: 'numeric',
-month: 'long',
-year: 'numeric'
-})
-let time = d.toLocaleTimeString(locale, {
-hour: 'numeric',
-minute: 'numeric',
-second: 'numeric'
-})
-let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) {
-process.send('uptime')
-_muptime = await new Promise(resolve => {
-process.once('message', resolve)
-setTimeout(resolve, 1000)
-}) * 1000
-}
-let { money } = global.db.data.users[m.sender]
-let muptime = clockString(_muptime)
-let uptime = clockString(_uptime)
-let totalreg = Object.keys(global.db.data.users).length
-let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-let replace = {
-'%': '%',
-p: _p, uptime, muptime,
-me: conn.getName(conn.user.jid),
+        let d = new Date(new Date + 3600000)
+        let locale = 'es'
+        let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+        let week = d.toLocaleDateString(locale, { weekday: 'long' })
+        let date = d.toLocaleDateString(locale, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+        })
+        let time = d.toLocaleTimeString(locale, {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        })
+        let _uptime = process.uptime() * 1000
+        let _muptime
+        if (process.send) {
+            process.send('uptime')
+            _muptime = await new Promise(resolve => {
+                process.once('message', resolve)
+                setTimeout(resolve, 1000)
+            }) * 1000
+        }
+        let { money } = global.db.data.users[m.sender]
+        let muptime = clockString(_muptime)
+        let uptime = clockString(_uptime)
+        let totalreg = Object.keys(global.db.data.users).length
+        let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+        let replace = {
+            '%': '%',
+            p: _p, uptime, muptime,
+            me: conn.getName(conn.user.jid),
 
-exp: exp - min,
-maxexp: xp,
-totalexp: exp,
-xp4levelup: max - exp,
+            exp: exp - min,
+            maxexp: xp,
+            totalexp: exp,
+            xp4levelup: max - exp,
 
-level, limit, weton, week, date, time, totalreg, rtotalreg, role,
-readmore: readMore
-}
-text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+            level, limit, weton, week, date, time, totalreg, rtotalreg, role,
+            readmore: readMore
+        }
+        text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let mentionedJid = [who]
-let username = conn.getName(who)
-let user = global.db.data.users[m.sender]
+        let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+        let mentionedJid = [who]
+        let username = conn.getName(who)
+        let user = global.db.data.users[m.sender]
 
-let pp = './storage/img/catalogo.jpg'
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let fsizedoc = '1'.repeat(10)
-let adReply = { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: { forwardingScore: fsizedoc, externalAdReply: { showAdAttribution: true, title: 'Título', body: '👋 ' + username, mediaUrl: 'url', description: 'Hola', previewType: 'PHOTO', thumbnail: await(await fetch('url')).buffer(), sourceUrl: 'url' }}}
+        let pp = './storage/img/catalogo.jpg'
+        let fkontak = { "key": { "participants": "0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` } }, "participant": "0@s.whatsapp.net" }
+        let fsizedoc = '1'.repeat(10)
+        let adReply = { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: { forwardingScore: fsizedoc, externalAdReply: { showAdAttribution: true, title: 'Menu +18', body: 'By WillZek' + username, mediaUrl: canal, description: null, previewType: 'PHOTO', thumbnail: await (await fetch('url')).buffer(), sourceUrl: canal }}}
 
-let menuA = `😏 Bienvenido *${username}*`.trim()
-let menuB = `╭┄〔 *Menú* 〕┄⊱
+        let menuA = `😏 Bienvenido *${username}*`.trim()
+        let menuB = `╭┄〔 *Menú* 〕┄⊱
 ┊ *${week}, ${date}*
 ┊ *Usuarios: ${Object.keys(global.db.data.users).length}* 
 ┊
@@ -124,16 +124,17 @@ let menuB = `╭┄〔 *Menú* 〕┄⊱
 `.trim()
 
 await conn.sendButton(m.chat, menuA, menuB, pp, [
-[lenguajeGB.smsBotonM1(), '.menu'], [lenguajeGB.smsBotonM2(), '/allmenu'], [lenguajeGB.smsBotonM3(), '#inventario']], fkontak, adReply, m) 
+    ['Menú', '.menu'], ['Todos los menús', '/allmenu'], ['Inventario', '#inventario']
+], fkontak, adReply, m)
 
 } catch (e) {
-await conn.sendButton(m.chat, `\nTítulo`, 'Error al ejecutar el comando. #report ' + usedPrefix + command, null, [[lenguajeGB.smsMensError1(), `#reporte *${usedPrefix + command}*`]], m)
-console.log(`❗❗Error ${usedPrefix + command} ❗❗`)
-console.log(e)        
+    await conn.sendButton(m.chat, `\nTítulo`, 'Error al ejecutar el comando. #report ' + usedPrefix + command, null, [['Reportar error', `#reporte *${usedPrefix + command}*`]], m)
+    console.log(`❗❗Error ${usedPrefix + command} ❗❗`)
+    console.log(e)        
 }}
 
-handler.help = ['infomenu'].map(v => v + 'able <option>')
-handler.tags = ['group', 'owner']
+handler.help = ['infomenu'].map(v => v + 'able <opción>')
+handler.tags = ['grupo', 'propietario']
 handler.command = /^(hornymenu)$/i
 handler.exp = 50
 export default handler
@@ -141,7 +142,8 @@ export default handler
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
