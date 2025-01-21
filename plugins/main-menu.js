ababec42a5212c9597ea74d243a40e29e6a3c5af
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-01-20 18:43:04 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -224,7 +224,7 @@ global.rcanal = {
     }
 
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
-    await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
+    await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null)
    //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
 
   } catch (e) {
@@ -233,9 +233,9 @@ global.rcanal = {
   }
 }
 
-handler.help = ['allmenu']
-handler.tags = ['main']
-handler.command = ['allmenu', 'menucompleto', 'menúcompleto', 'menú', 'menu'] 
+handler.help = ['allmenu'];
+handler.tags = ['main'];
+handler.command = ['allmenu', 'menucompleto', 'menúcompleto', 'menú', 'menu'] ;
 handler.register = true 
 export default handler
 
@@ -278,4 +278,4 @@ switch(hour){
   case 22: hour = 'una linda noche 🌙'; break;
   case 23: hour = 'una linda noche 🌃'; break;
 }
-  var greeting = "espero que tengas " + hour; 
+  var greeting = "espero que tengas " + hour;
