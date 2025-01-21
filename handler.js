4438bdd2852addd899d124ae873e4a9738119d79
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-01-21 10:50:19 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -422,7 +422,7 @@ global.dfail = (type, m, conn, usedPrefix) => {
         unreg: "> _*`‼️USUARIO NO REGISTRADO‼️`*_\n\n`Para Registrarse:`\n\n> .reg nombre.edad\n\n`Ejemplo:`\n\n> .reg Adri.17",
         restrict: "> _*`Comando desactivado por mi Owner`*_" 
     }[type]
-    if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))
+    if (msg) return conn.reply(m.chat, msg, m).then(_ => m.react('✖️'))
 }
 
 let file = global.__filename(import.meta.url, true)
